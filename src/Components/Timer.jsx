e7c4a2596a59abfe260e9d8cb2de40e2ea2af9b2
Joy Brar <joybrar2001@gmail.com>
2024-05-14 02:53:07 +0530

    Final before api

--- src/Components/Timer.jsx
+++ src/Components/Timer.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { MdOutlineTimer } from "react-icons/md";
+import GoogleAILogo from "/Google AI Logo.png";
 
 const Timer = ({ time }) => {
   return (
-    <div className='bg-white px-3 py-1 border-2 rounded-full flex justify-center items-center gap-2'>
-        <MdOutlineTimer className='mt-0.5' />
-        <p>00:{String(time).padStart(2,'0')}</p>
+    <div className='relative my-8'>
+      <div className='absolute left-[50%] -translate-x-[50%] -translate-y-[50%]'>
+        <div className='bg-white px-3 py-1 border-2 rounded-full flex justify-center items-center gap-2'>
+          <MdOutlineTimer className='mt-0.5' />
+          <p>00:{String(time).padStart(2, '0')}</p>
+        </div>
+      </div>
+      <img src={GoogleAILogo} alt="Gooogle AI" className='border-2 rounded-xl' />
     </div>
   )
 }
--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -11,7 +11,7 @@ import Loader from '../components/Loader';
 
 const QuizPage = () => {
   const status = useSelector((state) => state.quiz.status);
-  const quizData = useSelector((state) => state.quiz.quizData);
+  // const quizData = useSelector((state) => state.quiz.quizData);
 
   const currentIndex = useSelector((state) => state.quiz.index);
   const dispatch = useDispatch();
@@ -36,52 +36,11 @@ const QuizPage = () => {
   const [timer, setTimer] = useState(null);
   const [remainingTime, setRemainingTime] = useState(15);
 
-  useEffect(() => {
-    dispatch(fetchQuizData());
-  }, []);
-
   // useEffect(() => {
-  //   const requestOptions = {
-  //     method: "GET",
-  //     redirect: "follow"
-  //   };
-    
-  //   fetch("https://aws.testexperience.site/questions", requestOptions)
-  //     .then((response) => response.text())
-  //     .then((result) => console.log(result))
-  //     .catch((error) => console.error(error));
-  // }, [])
-
-  // const handleOptionClick = (selected) => {
-  //   setAnswered(true);
-  //   if (selected === correct) {
-  //     setCorrectAnswer(true);
-  //   } else {
-  //     setCorrectAnswer(false);
-  //   }
-  //   console.log("4 seconds left");
-  //   clearTimeout(timer);
-  //   setTimer(setTimeout(handleNextQuestion, 4000))
-  //   setRemainingTime(4);
-  // }
-
-  // useEffect(() => {
-  //   console.log("15 seconds left");
-  //   if(!answered && currentIndex < quizQuestions.length){
-  //     const newTimer = setTimeout(() => {
-  //       handleNextQuestion();
-  //     }, 15000);
-
-  //     setTimer(newTimer);
-
-  //     return () => clearTimeout(newTimer);
-  //   }
-  // }, [answered, currentIndex]);
-
-  // console.log(`${remainingTime} seconds left`);
+  //   dispatch(fetchQuizData());
+  // }, []);
 
   useEffect(() => {
-    // console.log("New question begins");
     if (!answered && currentIndex <= quizQuestions.length) {
       const interval = setInterval(() => {
         setRemainingTime((prevTime) => {
@@ -101,7 +60,6 @@ const QuizPage = () => {
   }, [answered, currentIndex]);
 
   const handleOptionClick = (selected) => {
-    // console.log("You clicked, timer change to 4 seconds");
     setAnswered(true);
     if (selected === correct) {
       setCorrectAnswer(true);
@@ -138,12 +96,7 @@ const QuizPage = () => {
       <QuizNavbar />
       <main className='w-full h-full mt-6 max-w-2xl mx-auto px-4 sm:px-6 md:px-8'>
 
-        <div className='relative my-8'>
-          <div className='absolute left-[50%] -translate-x-[50%] -translate-y-[50%]'>
-            <Timer time={remainingTime} />
-          </div>
-          <img src={GoogleAILogo} alt="Gooogle AI" className='border-2 rounded-xl' />
-        </div>
+        <Timer time={remainingTime} />
 
         <div className='flex flex-col gap-2 mt-2'>
           <div className='flex justify-between font-medium text-slate-400'>
@@ -158,6 +111,7 @@ const QuizPage = () => {
           <form className='flex flex-col gap-2.5 justify-start items-start w-full'>
             {options.map((option, i) => (
               <button
+                disabled={answered}
                 className={`w-full flex px-3 py-1 transition border-2
                 ${(answered && correctAnswer && option === correct) ? 'justify-between items-center border-green-500 border-2 rounded-lg bg-gradient-to-b from-green-100 via-green-100 to-transparent' : ''}
                 ${(answered && !correctAnswer) && option !== correct ? 'justify-between items-center border-red-500 border-2 rounded-lg bg-gradient-to-b from-red-100 via-red-100 to-transparent' : ''}
@@ -177,6 +131,7 @@ const QuizPage = () => {
           <button
             onClick={handleSkip}
             className='bg-gray-100 mx-auto text-sm px-5 py-2 rounded-full shadow-lg'
+            disabled={answered}
           >
             Skip to Next
           </button>
