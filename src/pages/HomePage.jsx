39c2024bc61bb6f68ebdb376dff3ced18fe2eeca
Joy Brar <joybrar2001@gmail.com>
2024-05-15 17:50:52 +0530

    Force Push

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,14 +3,6 @@ import { useNavigate } from 'react-router-dom'
 import GoogleLogo from '/Google Logo.png';
 import WelcomeScreenPoster from '/Welcome Screen Poster.png';
 import ImageLoader from '../Components/ImageLoader';
-import axios from 'axios';
-
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import { Autocomplete, TextField } from '@mui/material';
 
 const territories = [
   { label: "New York, NY"},
@@ -24,12 +16,6 @@ const territory_id = "New York, NY";
 
 const HomePage = () => {
   const navigate = useNavigate();
-
-  const [territory, setTerritory] = useState('');
-
-  const handleChange = (event) => {
-    setTerritory(event.target.value);
-  };
 
   return (
     <div className="bg-white min-h-screen w-full flex flex-col justify-start items-center">
@@ -45,16 +31,6 @@ const HomePage = () => {
           <h1 className='font-semibold text-2xl sm:text-3xl md:text-5xl -mt-0.5'>Pixel Quiz Challenge</h1>
           <p className='font-medium text-sm md:text-xl text-zinc-600 mt-3 leading-tight'>Play Pixel Quiz Challenge and win coupons and many more rewards...</p>
         </div>
-
-        {/* <div className='w-full max-w-md px-4 mx-auto mt-4 flex justify-center items-center'>
-          <Autocomplete 
-            disablePortal
-            id='combo-box-demo'
-            options={territories}
-            sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} label="Store"  />}
-          />
-        </div> */}
 
         <div className='max-w-2xl px-4 mx-auto mt-10 text-center'>
           <p className='text-xs md:text-lg tracking-tight leading-tight text-zinc-500 text-left'>
