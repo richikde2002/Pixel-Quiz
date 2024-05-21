1628f46e3a58d2fae8132a9874cfcbff66838c0f
Joy Brar <joybrar2001@gmail.com>
2024-05-21 18:44:47 +0530

    Changed Port to 3000

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    server: {
-      port: 3000,
-    },
+    port: 3000,
   },
   plugins: [react()],
 })
